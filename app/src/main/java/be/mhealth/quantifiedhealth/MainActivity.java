7afcb356e14149d7f8a56add3d77b7acfeed8037
Benjamin Friedrich <friedrich.benjamin@gmail.com>
2016-03-19 17:59:28 +0100

    created Profile settings

--- app/src/main/java/be/mhealth/quantifiedhealth/MainActivity.java
+++ app/src/main/java/be/mhealth/quantifiedhealth/MainActivity.java
@@ -1,10 +1,15 @@
 package be.mhealth.quantifiedhealth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -49,4 +54,15 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+    public void showProfileSettings(final View view){
+        final Intent intent = new Intent(this, ProfileSettingsActivity.class);
+        startActivity(intent);
+
+
+    }
+
+
 }
