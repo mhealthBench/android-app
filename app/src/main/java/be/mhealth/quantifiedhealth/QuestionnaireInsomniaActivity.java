24c3532b87f53bd5a9c89ebf042c68f2331d679f
stijn_coolbrandt <stijn.coolbrandt@gmail.com>
2016-03-20 01:36:58 +0100

    Added Questionnaire stuff

---
+++ app/src/main/java/be/mhealth/quantifiedhealth/QuestionnaireInsomniaActivity.java
@@ -0,0 +1,46 @@
+package be.mhealth.quantifiedhealth;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class QuestionnaireInsomniaActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insomnia_questionnaire);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.insomnia_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, QuestionnaireUrinaryIncontinenceActivity.class);
+                startActivity(intent);
+
+            }
+
+        });
+
+    }
+}
