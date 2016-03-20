e3399237f0d92e7992a7a3cf82188fbc47c8140b
Ken Dahm <kdahm@Kens-MacBook-Pro.local>
2016-03-20 14:40:08 +0100

    Double swipe right to advice view

--- app/src/main/java/be/mhealth/quantifiedhealth/DiaryActivity.java
+++ app/src/main/java/be/mhealth/quantifiedhealth/DiaryActivity.java
@@ -1,7 +1,11 @@
 package be.mhealth.quantifiedhealth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import be.mhealth.quantifiedhealth.listener.OnSwipeTouchListener;
 
 public class DiaryActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class DiaryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_diary);
 
         setTitle(R.string.title_activity_diary);
+
+        ((ImageView)findViewById(R.id.imgViewDiary)).setOnTouchListener(new OnSwipeTouchListener(DiaryActivity.this) {
+            @Override
+            public void onSwipeRight() {
+                final Intent intent = new Intent(DiaryActivity.this, AdviceActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
