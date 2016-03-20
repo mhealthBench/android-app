947a696f4a63c1469d816f7f494e15c9e0efc934
Ken Dahm <kdahm@Kens-MacBook-Pro.local>
2016-03-20 14:21:22 +0100

    Deleted advice entry activity and row adapter

--- app/src/main/java/be/mhealth/quantifiedhealth/AdviceActivity.java
+++ app/src/main/java/be/mhealth/quantifiedhealth/AdviceActivity.java
@@ -12,44 +12,11 @@ import java.util.ArrayList;
 
 public class AdviceActivity extends AppCompatActivity {
 
-    ArrayList<String> listItems = new ArrayList<String>();
-
-    ArrayAdapter<String> adapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice);
 
         setTitle(R.string.title_activity_advice);
-
-        /*final ListView lw = (ListView) findViewById(R.id.listAdvice2);
-        adapter=new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,
-                listItems);
-
-        final String[] entries = getResources().getStringArray(R.array.advice_entry_highlights);
-
-        adapter=new RowAdapter(this,
-                android.R.layout.simple_list_item_1,
-                entries);
-
-
-
-        lw.setAdapter(adapter);
-
-        // final String[] entries = getResources().getStringArray(R.array.advice_entry_highlights);
-        // adapter.addAll(entries);
-
-
-        lw.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(final AdapterView<?> adapterView, final View view,
-                                    final int position, final long id) {
-                final Intent intent = new Intent(AdviceActivity.this, AdviceEntryActivity.class);
-                intent.putExtra(AdviceEntryActivity.ENTRY_POSITION, position);
-                startActivity(intent);
-            }
-        });*/
     }
 }
